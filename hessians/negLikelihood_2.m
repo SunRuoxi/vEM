04feb6d20625b0dffc9f84e7081b5663f4747fde
Ruoxi Sun <Ruoxi@dyn-129-236-217-132.dyn.columbia.edu>
2017-02-24 22:40:19 -0500

    remove hessians folder

--- hessians/negLikelihood_2.m
+++
@@ -1,3 +0,0 @@
-function [out] = negLikelihood_2(x, w, y, c, baseline, one,Xt, Yt)
-out = -sum(-(baseline +w(1)*1/one *  1/(2*pi*(abs(c).^2))*exp(-(([Xt]-x(1)).^2 + ([Yt]-x(3)).^2)/(2*(c.^2)))+w(2)*1/one *  1/(2*pi*(abs(c).^2))*exp(-(([Xt]-x(2)).^2 + ([Yt]-x(4)).^2)/(2*(c.^2))))+[y].* log(baseline +w(1)*1/one *  1/(2*pi*(abs(c).^2))*exp(-(([Xt]-x(1)).^2 + ([Yt]-x(3)).^2)/(2*(c.^2)))+w(2)*1/one *  1/(2*pi*(abs(c).^2))*exp(-(([Xt]-x(2)).^2 + ([Yt]-x(4)).^2)/(2*(c.^2)))));
-end
